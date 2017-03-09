4eaa625b45e2c1a556c5b9d681e218fa91bc6b88
Alexander Herrfurth <alexander-herrfurth@web.de>
2017-03-09 14:56:44 +0100

    Added documentation to class members.

--- Yetibyte.Himalaya/GameElements/Scene.cs
+++ Yetibyte.Himalaya/GameElements/Scene.cs
@@ -10,9 +10,17 @@ namespace Yetibyte.Himalaya.GameElements {
 
         // Properties
 
-        public float TimeScale { get; set } = 1f;
+        /// <summary>
+        /// A global scaling value for time based operations to simulate the effect of slowing down or speeding up time. This value is passed
+        /// to the Update method of all objects that implement the IUpdate interface.
+        /// </summary>
+        public float TimeScale { get; set; } = 1f;
 
+        /// <summary>
+        /// A collection of all the GameEntities that currently live in this Scene.
+        /// </summary>
 		public List<GameEntity> GameEntities { get; protected set; }
+
 		public Queue<GameEntity> GameEntitiesToAdd { get; protected set; }
 		public Queue<GameEntity> GameEntitiesToRemove { get; protected set;}
 		
@@ -31,10 +39,16 @@ namespace Yetibyte.Himalaya.GameElements {
 		
 		// Methods
 		
+        /// <summary>
+        /// Responsible for loading content that is used in this Scene.
+        /// </summary>
 		public virtual void LoadContent() {
 						
 		}
 		
+        /// <summary>
+        /// Calls the Initialize method of each GameEntity within this Scene.
+        /// </summary>
 		public virtual void Initialize() {
 			
 			foreach(GameEntity gameEntity in GameEntities) {
@@ -45,6 +59,10 @@ namespace Yetibyte.Himalaya.GameElements {
 			
 		}
 		
+        /// <summary>
+        /// Calls the Update method of each GameEntity within this Scene.
+        /// </summary>
+        /// <param name="gameTime">Provides snapshot of current timing values.</param>
 		public virtual void Update(GameTime gameTime) {
 			
 			while(GameEntitiesToAdd.Count > 0) {
@@ -67,8 +85,13 @@ namespace Yetibyte.Himalaya.GameElements {
 			}
 						
 		}
-		
-		public virtual void Draw(SpriteBatch spriteBatch, GameTime gameTime) {
+
+        /// <summary>
+        /// Calls the Draw method of each GameEntity within this Scene.
+        /// </summary>
+        /// <param name="spriteBatch">The spritebatch to use for rendering.</param>
+        /// <param name="gameTime">Provides snapshot of current timing values.</param>
+        public virtual void Draw(SpriteBatch spriteBatch, GameTime gameTime) {
 			
 			foreach(GameEntity gameEntity in GameEntities) {
 				
@@ -78,7 +101,11 @@ namespace Yetibyte.Himalaya.GameElements {
 			}				
 			
 		}
-		
+		/// <summary>
+        /// Adds the given GameEntity to this Scene. Please note that the processing of newly added GameEntities will start in the next
+        /// Update interval.
+        /// </summary>
+        /// <param name="gameEntity">The GameEntity to add to the Scene.</param>
 		public void AddGameEntity(GameEntity gameEntity) {
 			
 			GameEntitiesToAdd.Enqueue(gameEntity);
@@ -86,6 +113,11 @@ namespace Yetibyte.Himalaya.GameElements {
 			
 		}
 		
+        /// <summary>
+        /// Removes the given GameEntity from this Scene. Please note that GameEntities will effectively be removed at the start of the next
+        /// Update interval.
+        /// </summary>
+        /// <param name="gameEntity">The GameEntity to remove from this Scene.</param>
 		public void RemoveGameEntity(GameEntity gameEntity) {
 			
 			GameEntitiesToRemove.Enqueue(gameEntity);
