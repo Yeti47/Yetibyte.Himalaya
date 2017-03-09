b7fec15ab5d1e04d2a578856f53f504d183ae0a8
Alexander Herrfurth <alexander-herrfurth@web.de>
2017-03-09 10:20:56 +0100

    Removed header template.

--- Yetibyte.Himalaya/Procedural/DungeonGenerator.cs
+++ Yetibyte.Himalaya/Procedural/DungeonGenerator.cs
@@ -1,12 +1,4 @@
-﻿/*
- * Created by SharpDevelop.
- * User: Alex
- * Date: 23.02.2017
- * Time: 09:46
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-using System;
+﻿using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using System.Diagnostics;
