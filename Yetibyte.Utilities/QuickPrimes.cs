6cf79293d200da7530dabc4cc334ddc3c254817c
Alexander Herrfurth <alexander-herrfurth@web.de>
2017-04-28 14:07:46 +0200

    added enumerator for prime numbers

---
+++ Yetibyte.Utilities/QuickPrimes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yetibyte.Utilities {
+
+    /// <summary>
+    /// A quick access to a predefined set of prime numbers.
+    /// </summary>
+    public enum QuickPrimes
+    {
+
+        P3 = 3, P7 = 7, P11 = 11, P17 = 17, P23= 23, P29= 29, P37 = 37,
+        P47 = 47, P59 = 59, P71 = 71, P89 = 89, P107 = 107, P131 = 131,
+        P163 = 163, P197 = 197, P239 = 239, P293 = 293, P353 = 353,
+        P1103 = 1103, P1327 = 1327, P1597 = 1597, P1931 = 1931,
+        P4861 = 4861, P5839 = 5839, P7013 = 7013, P8419 = 8419,
+        P10103 = 10103, P90523 = 90523, P7199369 = 7199369
+
+    }
+}
