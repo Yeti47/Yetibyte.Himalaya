b5c145530849598bd9084ec24a040ee80920ef3e
Alexander Herrfurth <alexander-herrfurth@web.de>
2017-05-19 09:15:08 +0200

    Added method that returns the result of AngleBetween clamped between pi and -pi.

--- Yetibyte.Himalaya/Vector2Helper.cs
+++ Yetibyte.Himalaya/Vector2Helper.cs
@@ -10,7 +10,7 @@ namespace Yetibyte.Himalaya {
     public static class Vector2Helper {
 
         /// <summary>
-        /// Calculates the angle between the the given <see cref="Vector2"/>s in radians.
+        /// Calculates the angle between the given <see cref="Vector2"/>s in radians.
         /// </summary>
         /// <param name="vectorA">The first vector.</param>
         /// <param name="vectorB">The second vector.</param>
@@ -20,6 +20,14 @@ namespace Yetibyte.Himalaya {
             return (float)(Math.Atan2(vectorB.Y, vectorB.X) - Math.Atan2(vectorA.Y, vectorA.X));
 
         }
+
+        /// <summary>
+        /// Calculates the angle between the given <see cref="Vector2"/>s in radians and constrains the result to lie between π and -π.
+        /// </summary>
+        /// <param name="vectorA">The first vector.</param>
+        /// <param name="vectorB">The second vector.</param>
+        /// <returns>The angle betwwen the two given vectors in radians clamped between π and -π.</returns>
+        public static float AngleBetweenWrapped(Vector2 vectorA, Vector2 vectorB) => MathHelper.WrapAngle(AngleBetween(vectorA, vectorB));
                 
     }
 
